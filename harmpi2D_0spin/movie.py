588fb540ca4c6ca9a75f4f70de4701add689f511
samuelsolod <solodsamuel@gmail.com>
2025-01-05 18:30:32 -0600

    Delete harmpi2D_0spin directory

--- harmpi2D_0spin/movie.py
+++
@@ -1,6 +0,0 @@
-exec(open('/scratch/qkt4685/harmpi3D_0.9spin/harm_script.py').read())
-mkmov_simple(starti = 0,endi = 2000)
-import subprocess
-
-command = "ffmpeg -i frame%03d.png -vcodec mpeg4 -qmax 5 movie.mp4"
-subprocess.run(command, shell=True, check=True)
